ab5a68ff1dc7bab6b782e8729b84ac037302d928
PostAdam <35382123+PostAdam@users.noreply.github.com>
2018-12-13 15:05:10 +0100

    Revert "deleted expired directories"
    This reverts commit be04c70f5516924eecc5ff0e90fb2c7900c67c25.
    
    	new file:   FileLogger/FileLogger.cs
    	new file:   FileLogger/FileLogger.csproj
    	new file:   FileLogger/Properties/AssemblyInfo.cs

---
+++ DataBaseSerializationSurrogate/MetadataSurrogates/MethodMetadataSurrogate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Model.Reflection.Enums;
+using Model.Reflection.MetadataModels;
+
+namespace DataBaseSerializationSurrogate.MetadataSurrogates
+{
+    [DataContract( IsReference = true, Name = "MethodMetadata" )]
+    public class MethodMetadataSurrogate
+    {
+        #region Constructor
+
+        public MethodMetadataSurrogate( MethodMetadata methodMetadata )
+        {
+            Name = Name;
+            Extension = methodMetadata.Extension;
+            ReturnType = TypeMetadataSurrogate.EmitSurrogateTypeMetadata( methodMetadata.ReturnType );
+            MethodAttributes = CollectionTypeAccessor.GetTypesMetadata( methodMetadata.MethodAttributes );
+            Parameters = CollectionTypeAccessor.GetParametersMetadata( methodMetadata.Parameters );
+            GenericArguments = CollectionTypeAccessor.GetTypesMetadata( methodMetadata.GenericArguments );
+            Modifiers = methodMetadata.Modifiers;
+        }
+
+        #endregion
+
+        #region Properties
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public bool Extension { get; set; }
+
+        [DataMember]
+        public TypeMetadataSurrogate ReturnType { get; set; }
+
+        [DataMember]
+        public IEnumerable<TypeMetadataSurrogate> MethodAttributes { get; set; }
+
+        [DataMember]
+        public IEnumerable<ParameterMetadataSurrogate> Parameters { get; set; }
+
+        [DataMember]
+        public IEnumerable<TypeMetadataSurrogate> GenericArguments { get; set; }
+
+        [DataMember]
+        public Tuple<AccessLevel, AbstractEnum, StaticEnum, VirtualEnum> Modifiers { get; set; }
+
+        #endregion
+
+        public MethodMetadata GetOriginalMethodMetadata()
+        {
+            return new MethodMetadata()
+            {
+                Name = Name,
+                Extension = Extension,
+                ReturnType = ReturnType?.EmitOriginalTypeMetadata(),
+                MethodAttributes = CollectionOriginalTypeAccessor.GetOriginalTypesMetadata( MethodAttributes ),
+                Parameters = CollectionOriginalTypeAccessor.GetOriginalParametersMetadata( Parameters ),
+                GenericArguments = CollectionOriginalTypeAccessor.GetOriginalTypesMetadata( GenericArguments ),
+                Modifiers = Modifiers
+            };
+        }
+    }
+}
