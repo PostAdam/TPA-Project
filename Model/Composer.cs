a0bb2e4ff25c9a97cf2aed8798bee63a08b1a7aa
PostAdam <35382123+PostAdam@users.noreply.github.com>
2019-01-18 03:01:46 +0100

    fixed couple bugs

--- Model/Composer.cs
+++ Model/Composer.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
@@ -17,72 +18,33 @@ namespace Model
 {
     public class Composer
     {
-        [ImportMany( typeof( IRepository ) )]
-        public IEnumerable<Lazy<IRepository, IDictionary<string, object>>> Repositories;
-
-        [ImportMany( typeof( ITrace ) )] public IEnumerable<Lazy<ITrace, IDictionary<string, object>>> Loggers;
-
-        public ITrace Logger;
-        public IRepository Repository;
+        #region Constructor
 
         public Composer()
         {
             Compose();
             SetUpDataDirectory();
-            string mdfFilePath = ( string ) AppDomain.CurrentDomain.GetData("DataDirectory");
-            var filename = System.IO.Path.Combine(mdfFilePath, "DataBase.mdf");
-            if (!System.IO.File.Exists(filename))
+            string mdfFilePath = ( string ) AppDomain.CurrentDomain.GetData( "DataDirectory" );
+            var filename = Path.Combine( mdfFilePath, "DataBase.mdf" );
+            if ( !File.Exists( filename ) )
             {
-                Task.Run(CreateMdfDatabaseFile).Wait();
+                Task.Run( CreateMdfDatabaseFile ).Wait();
             }
-            LoadRepository();
+
             LoadLogger();
+            LoadRepository();
         }
 
-        private void SetUpDataDirectory()
-        {
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string dataDirectory = baseDirectory.Remove( baseDirectory.Length - ( "WPF\\bin\\Debug".Length + 1 ) );
-            dataDirectory += "DataBase";
-            AppDomain.CurrentDomain.SetData( "DataDirectory", dataDirectory );
-        }
+        #endregion
 
-        private async Task CreateMdfDatabaseFile()
-        {
-            using ( SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True") )
-            {
-                using ( SqlCommand command = new SqlCommand() )
-                {
-                    string path = (string) AppDomain.CurrentDomain.GetData( "DataDirectory" );
-                    string stringCommand = "CREATE DATABASE MyDatabase ON PRIMARY " +
-                                 "(NAME = Database_Data, " +
-                                 $"FILENAME = '{path}\\DataBase.mdf') " +
-                                 "LOG ON (NAME = MyDatabase_Log, " +
-                                 $"FILENAME = '{path}\\DataBase.ldf')";
+        [ImportMany( typeof( IRepository ) )]
+        public IEnumerable<Lazy<IRepository, IDictionary<string, object>>> Repositories;
 
-                    command.CommandText = stringCommand;
-                    command.Connection = connection;
-                    try
-                    {
-                        connection.Open();
-                        await command.ExecuteNonQueryAsync();
-                    }
-                    catch ( Exception e )
-                    {
-                        Console.WriteLine( e );
-                    }
-                }
-            }
-        }
+        [ImportMany( typeof( ITrace ) )]
+        public IEnumerable<Lazy<ITrace, IDictionary<string, object>>> Loggers;
 
-        private string GetConnectionString()
-        {
-            Configuration appConfig =
-                ConfigurationManager.OpenExeConfiguration( Assembly.GetExecutingAssembly().Location );
-            string connectionString = appConfig.ConnectionStrings
-                .ConnectionStrings["DataBase.Properties.Settings.DataBaseConnectionString"].ConnectionString;
-            return connectionString;
-        }
+        public ITrace Logger;
+        public IRepository Repository;
 
         public void Compose()
         {
@@ -102,48 +64,9 @@ namespace Model
             }
             catch ( Exception exception ) when ( exception is ReflectionTypeLoadException )
             {
-                ReflectionTypeLoadException typeLoadException = (ReflectionTypeLoadException) exception;
+                ReflectionTypeLoadException typeLoadException = ( ReflectionTypeLoadException ) exception;
                 Exception[] loaderExceptions = typeLoadException.LoaderExceptions;
                 loaderExceptions.ToList().ForEach( ex => Console.WriteLine( ex.StackTrace ) );
-            }
-        }
-
-        private List<DirectoryCatalog> GetDirectoryCatalogs()
-        {
-            NameValueCollection dllsPaths = ConfigurationManager.GetSection( "plugins" ) as NameValueCollection;
-
-            List<DirectoryCatalog> directoryCatalogs = new List<DirectoryCatalog>();
-            foreach ( string dllPath in dllsPaths.AllKeys )
-            {
-                directoryCatalogs.Add( new DirectoryCatalog( dllPath, "*.dll" ) );
-            }
-
-            return directoryCatalogs;
-        }
-
-        private void LoadRepository()
-        {
-            string repositoryType = ConfigurationManager.AppSettings["repositoryType"];
-            Repository = Repositories
-                .FirstOrDefault( repository => (string) repository.Metadata["destination"] == repositoryType )?.Value;
-        }
-
-        private void LoadLogger()
-        {
-            string loggerType = ConfigurationManager.AppSettings["loggerType"];
-            Logger = Loggers.FirstOrDefault( logger => (string) logger.Metadata["destination"] == loggerType )?.Value;
-            if ( Logger != null )
-            {
-                string logLevel = ConfigurationManager.AppSettings["logLevel"];
-
-                if ( int.TryParse( logLevel, out int level ) )
-                {
-                    Logger.Level = (LogLevel) level;
-                }
-                else
-                {
-                    Logger.Level = LogLevel.Warning;
-                }
             }
         }
 
@@ -158,5 +81,92 @@ namespace Model
         {
             await Repository.Write( assemblyMetadata.GetOriginalAssemblyMetadata(), cancellationTokenSource );
         }
+
+        #region Private
+
+        private void SetUpDataDirectory()
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string dataDirectory = baseDirectory.Remove( baseDirectory.Length - ( "WPF\\bin\\Debug".Length + 1 ) );
+            dataDirectory += "DataBase";
+            AppDomain.CurrentDomain.SetData( "DataDirectory", dataDirectory );
+        }
+
+        private async Task CreateMdfDatabaseFile()
+        {
+            using ( SqlConnection connection =
+                new SqlConnection(
+                    "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True" ) )
+            {
+                using ( SqlCommand command = new SqlCommand() )
+                {
+                    string path = ( string ) AppDomain.CurrentDomain.GetData( "DataDirectory" );
+                    string stringCommand = GetStringCommand( path );
+
+                    command.CommandText = stringCommand;
+                    command.Connection = connection;
+                    try
+                    {
+                        connection.Open();
+                        await command.ExecuteNonQueryAsync();
+                    }
+                    catch ( Exception e )
+                    {
+                        Console.WriteLine( e );
+                    }
+                }
+            }
+        }
+
+        private static string GetStringCommand( string path )
+        {
+            return "CREATE DATABASE MyDatabase ON PRIMARY " +
+                   "(NAME = Database_Data, " +
+                   $"FILENAME = '{path}\\DataBase.mdf') " +
+                   "LOG ON (NAME = MyDatabase_Log, " +
+                   $"FILENAME = '{path}\\DataBase.ldf')";
+        }
+
+        private List<DirectoryCatalog> GetDirectoryCatalogs()
+        {
+            NameValueCollection dllsPaths = ConfigurationManager.GetSection( "plugins" ) as NameValueCollection;
+
+            List<DirectoryCatalog> directoryCatalogs = new List<DirectoryCatalog>();
+            if ( dllsPaths != null )
+                foreach ( string dllPath in dllsPaths.AllKeys )
+                {
+                    directoryCatalogs.Add( new DirectoryCatalog( dllPath, "*.dll" ) );
+                }
+
+            return directoryCatalogs;
+        }
+
+        private void LoadRepository()
+        {
+            string repositoryType = ConfigurationManager.AppSettings[ "repositoryType" ];
+            Repository = Repositories
+                .FirstOrDefault( repository => ( string ) repository.Metadata[ "destination" ] == repositoryType )?.Value;
+        }
+
+        private void LoadLogger()
+        {
+            string loggerType = ConfigurationManager.AppSettings[ "loggerType" ];
+            Logger = Loggers.FirstOrDefault( logger => ( string ) logger.Metadata[ "destination" ] == loggerType )?.Value;
+            if ( Logger != null )
+            {
+                string logLevel = ConfigurationManager.AppSettings[ "logLevel" ];
+
+                if ( int.TryParse( logLevel, out int level ) )
+                {
+                    Logger.Level = ( LogLevel ) level;
+                }
+                else
+                {
+                    Logger.Level = LogLevel.Warning;
+                }
+            }
+        } 
+
+        #endregion
     }
 }
